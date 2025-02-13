e50e6dceacddfee6187af6fd14eae32496cea22e
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-02-13 09:23:57 +0700

    backend entities

--- dana-hotel/src/main/java/com/hdoan/danahotel/model/BookedRoom.java
+++ dana-hotel/src/main/java/com/hdoan/danahotel/model/BookedRoom.java
@@ -1,8 +1,145 @@
 package com.hdoan.danahotel.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 /**
  * @author Huy
  */
 
+@Entity
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int NumOfAdults;
+
+    @Column(name = "children")
+    private int NumOfChildren;
+
+    @Column(name = "total_Guest")
+    private int totalNumberOfGuest;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public BookedRoom() {
+    }
+
+    public BookedRoom(LocalDate checkInDate, LocalDate checkOutDate, String guestFullName, String guestEmail,
+                      int numOfAdults, int numOfChildren, Room room) {
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.guestFullName = guestFullName;
+        this.guestEmail = guestEmail;
+        NumOfAdults = numOfAdults;
+        NumOfChildren = numOfChildren;
+        this.room = room;
+    }
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getGuestFullName() {
+        return guestFullName;
+    }
+
+    public void setGuestFullName(String guestFullName) {
+        this.guestFullName = guestFullName;
+    }
+
+    public String getGuestEmail() {
+        return guestEmail;
+    }
+
+    public void setGuestEmail(String guestEmail) {
+        this.guestEmail = guestEmail;
+    }
+
+    public int getNumOfAdults() {
+        return NumOfAdults;
+    }
+
+    public int getNumOfChildren() {
+        return NumOfChildren;
+    }
+
+    public int getTotalNumberOfGuest() {
+        return totalNumberOfGuest;
+    }
+
+    public void setTotalNumberOfGuest(int totalNumberOfGuest) {
+        this.totalNumberOfGuest = totalNumberOfGuest;
+    }
+
+    public String getBookingConfirmationCode() {
+        return bookingConfirmationCode;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumberOfGuest = this.NumOfAdults + this.NumOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        NumOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        NumOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
